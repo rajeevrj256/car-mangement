4a1587e56e45a76712f2cb8e253d6eda4e88758d
rajeevrj256 <rjrajeev5918@gmail.com>
2024-11-18 01:22:48 +0530

    d

--- frontend/src/components/custom/header.jsx
+++ frontend/src/components/custom/header.jsx
@@ -76,7 +76,7 @@ function Header() {
 
     return (
         <div className='p-2 shadow-sm flex justify-between items-center px-3'>
-            <img src="/logo.svg" alt="logo" />
+            <img src="" alt="logo" />
             <div>
                 {users ?
                     <div className='flex items-center gap-3'>
